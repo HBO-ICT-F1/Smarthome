b97150d8b85ca1e13e5212c030ac2440df353d0c
Robert <robertkremer.frl@gmail.com>
2020-11-09 11:46:29 +0100

    Initial commit

---
+++ app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nhlstenden.smarthome.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nhlstenden.smarthome.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
