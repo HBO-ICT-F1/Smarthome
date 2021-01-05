bd41b659419976fcaccb965ce6f120b2dc8ffcdf
TheDutchScorpion <kevinsliedrecht2000@gmail.com>
2021-01-05 16:35:35 +0100

    Add custom toolbar with menu

--- app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
+++ app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nhlstenden.smarthome.activities
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.nhlstenden.smarthome.R
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 
         // Request required permissions
         requestPermissions(arrayOf(INTERNET), 0)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -27,5 +30,11 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         }
 
         println("Permissions accepted")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
