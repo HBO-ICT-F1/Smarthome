7fc7218a97c87db8b0030cd2e5cfc4d2786803ef
Kevin <kevinsliedrecht2000@gmail.com>
2021-01-08 18:24:59 +0100

    Added custom dialogs and toolbar (#11)
    * Add custom toolbar with menu
    
    * Add button generation with dialog alert to show arduino status
    
    * Updated dependencies
    Updated android build tools from 30.0.2 to 30.0.3
    Updated kotlin from 1.4.20 to 1.4.21
    Updated appcompat from 1.1.0 to 1.2.0
    
    Signed-off-by: Robert <robertkremer.frl@gmail.com>
    
    * Added view binding to activity_main.xml
    Added view binding to main activity
    Added TODOs to MainActivity.kt
    Removed outdated connection test code
    Removed deprecated kotlin-android-extensions plugin
    
    Signed-off-by: Robert <robertkremer.frl@gmail.com>
    
    * Add arduino dialog to add a arduino
    Add missing string values
    
    * Add alarm toggle button and listener
    Add arduino submit button listener
    Add loading textView with data by code
    
    * Minor improvements
    Removed unused library
    Changed theme to use the apps theme again
    
    Signed-off-by: Robert <robertkremer.frl@gmail.com>
    
    * Update main_menu.xml
    
    Co-authored-by: Robert <robertkremer.frl@gmail.com>
    Co-authored-by: Robert <32477930+R0b3rt1337@users.noreply.github.com>

--- app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
+++ app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
@@ -1,19 +1,42 @@
 package com.nhlstenden.smarthome.activities
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.nhlstenden.smarthome.R
 import com.nhlstenden.smarthome.connection.INTERNET
+import com.nhlstenden.smarthome.databinding.ActivityMainBinding
+import com.nhlstenden.smarthome.databinding.AddArduinoDialogBinding
+import com.nhlstenden.smarthome.databinding.InfoDialogBinding
 
+/**
+ * Smarthome app main activity
+ *
+ * @author Kevin
+ * @author Robert
+ * @since 1.0
+ */
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+    /** Binding for this activity */
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         // Request required permissions
         requestPermissions(arrayOf(INTERNET), 0)
+
+        // TODO: Load Arduino's from a database
+        for (x in 0..10) {
+            // Create buttons for all arduino's
+            addArduino()
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -27,5 +50,56 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         }
 
         println("Permissions accepted")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_arduino) {
+            val addDialog = AddArduinoDialogBinding.inflate(layoutInflater)
+            val alertDialog = AlertDialog.Builder(this@MainActivity)
+            addDialog.submitButton.setOnClickListener {
+                // TODO: Add DB save
+                println("add arduino")
+            }
+            alertDialog.setView(addDialog.root)
+            alertDialog.create().show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    /** Adds an Arduino entry to the layout */
+    private fun addArduino() {
+        // Create buttons for all arduino's
+        val button = Button(this).apply {
+            setOnClickListener {
+                // TODO: Use data of arduino's
+                val infoDialog = InfoDialogBinding.inflate(layoutInflater)
+                infoDialog.name.text = "Arduino"
+                infoDialog.ip.text = "127.0.0.1"
+                infoDialog.temp.text = "30 °C"
+                infoDialog.humidity.text = "40%"
+                infoDialog.alarm.text = "actief"
+                infoDialog.toggleAlarm.setOnClickListener {
+                    // TODO: Add api call to toggle alarm
+                    println("toggle alarm")
+                }
+                val alertDialog = AlertDialog.Builder(this@MainActivity)
+                alertDialog.setView(infoDialog.root)
+                alertDialog.create().show()
+            }
+
+            height = 160
+            text = "test"
+        }
+
+        // Add button to layout
+        binding.layout.addView(button)
     }
 }
