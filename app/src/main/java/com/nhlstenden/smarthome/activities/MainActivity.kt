ad46b0fa47ac344698e98aab525dbeedda8e5ce6
Robert <robertkremer.frl@gmail.com>
2021-01-08 02:53:01 +0100

    Added view binding to activity_main.xml Added view binding to main activity Added TODOs to MainActivity.kt Removed outdated connection test code Removed deprecated kotlin-android-extensions plugin
    Signed-off-by: Robert <robertkremer.frl@gmail.com>

--- app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
+++ app/src/main/java/com/nhlstenden/smarthome/activities/MainActivity.kt
@@ -4,42 +4,36 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Button
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.nhlstenden.smarthome.R
-import com.nhlstenden.smarthome.connection.Connection
 import com.nhlstenden.smarthome.connection.INTERNET
-import com.nhlstenden.smarthome.connection.PacketType
-import com.nhlstenden.smarthome.connection.packets.PacketRoomData
+import com.nhlstenden.smarthome.databinding.ActivityMainBinding
+import com.nhlstenden.smarthome.databinding.InfoDialogBinding
 
+/**
+ * Smarthome app main activity
+ *
+ * @author Kevin
+ * @author Robert
+ * @since 1.0
+ */
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+    /** Binding for this activity */
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         // Request required permissions
         requestPermissions(arrayOf(INTERNET), 0)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        val linearLayout = findViewById<LinearLayout>(R.id.linear_layout)
-
-        val alertDialog = AlertDialog.Builder(this@MainActivity).create()
-        val infoDialog = layoutInflater.inflate(R.layout.info_dialog, null)
-        alertDialog.setView(infoDialog)
-
+        // TODO: Load Arduino's from a database
         for (x in 0..10) {
-            val button = Button(this)
-            button.apply {
-                height = 160
-                text = "test"
-            }
-            button.setOnClickListener {
-                alertDialog.show()
-            }
-            linearLayout.addView(button)
+            // Create buttons for all arduino's
+            addArduino()
         }
     }
 
@@ -54,19 +48,31 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         }
 
         println("Permissions accepted")
-        Thread {
-            val connection = Connection.connect("185.35.35.20", 6001) ?: return@Thread
-            connection.register(PacketType.RoomData) { it: PacketRoomData ->
-                println(it.temperature);
-            }
-
-            connection.run()
-        }.start()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main_menu, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /** Adds an Arduino entry to the layout */
+    private fun addArduino() {
+        // Create buttons for all arduino's
+        val button = Button(this).apply {
+            setOnClickListener {
+                // TODO: Use data of arduino's
+                val infoDialog = InfoDialogBinding.inflate(layoutInflater)
+                val alertDialog = AlertDialog.Builder(this@MainActivity)
+                alertDialog.setView(infoDialog.root)
+                alertDialog.create().show()
+            }
+
+            height = 160
+            text = "test"
+        }
+
+        // Add button to layout
+        binding.layout.addView(button)
     }
 }
